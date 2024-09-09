64a56d4f3b7cac0b3c5569dd4202d15cebe7f1dc
Luca Laurent <lucalaurent5@gmail.com>
2024-09-09 20:12:51 +0100

    Erase works and replace with json works

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,17 +1,43 @@
-    let works = document.querySelector('.gallery');
-works.innerHTML = '';
+function eraser(selector) {
+    let works = document.querySelector(selector);
+    works.innerHTML = '';
+}
+
 
 async function getWorks() {
-    const projects = await fetch("https://localhost:5678/api/works");
-}
-async function displayWorks() {
-    const projects = await getWorks();
-    console.log("avec index");
+    const answer = await fetch("http://localhost:5678/api/works");
+    return await answer.json();
+  }
+async function displayWorks(projects) {
+    // Check if projects is undefined or not an array
+    if (!projects || !Array.isArray(projects)) {
+        console.error("No projects data or the response is not an array");
+        return; // Stop the function if projects is invalid
+    }
+    let display = document.querySelector('.gallery');
+ 
+    console.log(projects);
     for (let i = 0; i < projects.length; i++) {
         console.log(projects[i]);
+    
+        displayWorkInGallery(projects[i], display);
     }
 }
-
-let display = document.querySelector('.gallery');
-let newWorks = displayWorks();
-display.innerHTML = newWorks;
+async function displayWorkInGallery(project, root) {
+    console.log(project);
+    const container = document.createElement('figure');
+    const work = document.createElement('figure');
+    const img = document.createElement('img');
+    work.textContent = project.title;
+    img.src = project.imageUrl;
+    img.alt = project.title;
+    root.appendChild(container);
+    container.appendChild(img);
+    container.appendChild(work);
+}
+async function main() {
+    const projects = await getWorks();
+    eraser('.gallery');
+    displayWorks(projects);
+}
+main();
