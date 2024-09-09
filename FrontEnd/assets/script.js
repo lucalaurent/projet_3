1bdd3447055be50e1fa758f927e4eb3854099225
Luca Laurent <lucalaurent5@gmail.com>
2024-09-09 17:27:14 +0100

    premiere tentative de display works

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,2 +1,17 @@
-let works = document.querySelectorAll('.gallery');
-works.figure.innerHTML = 'New Content';
+    let works = document.querySelector('.gallery');
+works.innerHTML = '';
+
+async function getWorks() {
+    const projects = await fetch("https://localhost:5678/api/works");
+}
+async function displayWorks() {
+    const projects = await getWorks();
+    console.log("avec index");
+    for (let i = 0; i < projects.length; i++) {
+        console.log(projects[i]);
+    }
+}
+
+let display = document.querySelector('.gallery');
+let newWorks = displayWorks();
+display.innerHTML = newWorks;
