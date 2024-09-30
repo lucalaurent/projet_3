3b2ab0bbf92d1a724ab8c5998ec6456924b8be02
Luca Laurent <lucalaurent5@gmail.com>
2024-09-30 20:07:57 +0100

    Creation du JS pour le login form

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,19 +1,52 @@
+function formReset() {
+    const form = document.getElementById('myForm');
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        validateForm();
+    })
+};
+async function validateForm() {
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
 
-const form = document.getElementById('myForm');
-const emailInput = document.getElementById('email');
-const passwordInput = document.getElementById('password');
-
-function formValidation('submit', e => {
-    e.preventdefault();
-let email = emailInput.value;
-let password = passwordInput.value;
-
-if (email.trim() === '' || password.trim() === '') {
-    alert('Please fill in all fields');
-    return;
+    //Checker expression reguliere pour email. 
+    
+    
+    if (email === '' || password === '') {
+        alert('Incorrect email or password! Please try again.  ');
+        return false;
+    }
+    
+    let response = await fetch('http://localhost:5678/api/users/login',
+        {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                email: email,
+                password: password,
+            }),
+        })
+    if (response.ok) {
+        let data = await response.json();
+        let token = data.token;
+        localStorage.setItem('token', token);
+    }
+    else {
+        if (response.status === 401) {
+            alert('Mot de Passe incorrect!');
+        }
+        if (response.status === 404) {
+            alert('Email non trouvé!');
+        }
+    }
 }
-else
-    SubmitEvent(submit);
-) {
+        
+// if logged in delete Login text and add logout text. 
 
-}
+    function main() {
+        formReset();
+    }
+   
+main();
