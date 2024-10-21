f27b68d0449a99856b8bdd50f98428a285647076
Luca Laurent <lucalaurent5@gmail.com>
2024-10-21 14:12:27 +0100

    Fix fermeture de la modal => ok

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -9,7 +9,7 @@ async function getWorks() {
 async function getCategories() {
     const answer = await fetch("http://localhost:5678/api/categories");
     return await answer.json();
-  }
+}
 async function displayWorks(projects) {
     // Check if projects is undefined or not an array
     if (!projects || !Array.isArray(projects)) {
@@ -17,18 +17,18 @@ async function displayWorks(projects) {
         return; // Stop the function if projects is invalid
     }
     let display = document.querySelector('.gallery');
- 
+
     console.log(projects);
     for (let i = 0; i < projects.length; i++) {
-      //  console.log(projects[i]);
-    
+        //  console.log(projects[i]);
+
         displayWorkInGallery(projects[i], display);
     }
 }
 async function displayWorkInGallery(project, root) {
     //console.log(project);
     const container = document.createElement('figure');
-    const work = document.createElement('figure');
+    const work = document.createElement('figcaption');
     const img = document.createElement('img');
     work.textContent = project.title;
     img.src = project.imageUrl;
@@ -42,22 +42,22 @@ function createbutton(categories) {
     const filters = document.createElement('div');
     filters.classList.add('filters');
     portfolio.appendChild(filters);
-    buttoncreation(filters, 'Tous',0);
+    buttoncreation(filters, 'Tous', 0);
     for (let i = 0; i < categories.length; i++) {
-      buttoncreation(filters, categories[i].name, categories[i].id);
+        buttoncreation(filters, categories[i].name, categories[i].id);
     }
 }
 function buttoncreation(filters, name, catId) {
     const btn = document.createElement('button');
     btn.textContent = name;
     filters.appendChild(btn);
-    btn.addEventListener('click',() => filterWorks(catId)  );
+    btn.addEventListener('click', () => filterWorks(catId));
 }
 
 async function filterWorks(buttonId) {
     const works = await getWorks();
     eraser('.gallery');
-    if (buttonId === 0 ) {
+    if (buttonId === 0) {
         displayWorks(works);
     }
     else {
@@ -65,11 +65,51 @@ async function filterWorks(buttonId) {
         displayWorks(filteredWorks);
     }
 }
+let modal = null;
+
+const openModal = function (e) {
+    document.querySelector('.js-modal').addEventListener('click', openModal)
+    e.preventDefault();
+    const target = document.querySelector(e.target.getAttribute('href'))
+    console.log(target);
+    target.style.display = 'flex';
+    target.removeAttribute('aria-hidden');
+    target.setAttribute('aria-modal', 'true');
+    modal = target;
+    modal.addEventListener('click', closeModal);
+    modal.querySelector('.close-modal').addEventListener('click', closeModal);
+}
+
+const closeModal = function (e) {
+    console.log(modal);
+    e.preventDefault();
+    modal.style.display = 'none';
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    modal.removeEventListener('click', closeModal);
+    modal.querySelector('.close-modal').removeEventListener('click', closeModal);
+    modal = null;
+}
+
+function modalContent() {
+    const work = getWorks(); //obtention des Works
+    const container = document.createElement('figure');
+    const img = document.createElement('img');
+    container.appendChild(img);
+    container.appendChild(work);
+}
+    //Ajouter la corbeille,
+    // Ajouter event listener sur chacunne des corbeilles pour supprimer les Works
+    // Current target pour supprimer un work de la modal ET du front avec "Fetch Delete : ID" 
+    //Ajouter le bouton pour fermer la modal ET event listener pour fermer la modal 
+    
+
 async function main() {
     const projects = await getWorks();
     const categories = await getCategories();
     eraser('.gallery');
     createbutton(categories);
     displayWorks(projects);
+    openModal();
 }
 main();
